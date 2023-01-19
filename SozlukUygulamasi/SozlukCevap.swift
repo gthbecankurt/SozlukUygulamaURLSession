eb175aa532b7cf2ac91cf52d9fb83ada6ece5535
= <=>
2023-01-19 23:39:41 +1200

    UrlSession ile yapılan çalışma commit edildi

---
+++ SozlukUygulamasi/SozlukCevap.swift
@@ -0,0 +1,13 @@
+//
+//  SozlukCevap.swift
+//  SozlukUygulamasi
+//
+//  Created by Kasım Adalan on 6.08.2019.
+//  Copyright © 2019 info. All rights reserved.
+//
+
+import Foundation
+
+class SozlukCevap:Codable {
+    var kelimeler:[Kelimeler]?
+}
